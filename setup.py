e09d0bb8df96e50de7e2f9c2ad07200241fa6f83
TakesxiSximada <takesxi.sximada@gmail.com>
2015-11-27 19:05:10 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,51 +1,59 @@
-##############################################################################
-#
-# Copyright (c) 2007 Zope Foundation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
-"""Setup for zc.recipe.egg package
-"""
-
-version = '2.0.3'
-
+# -*- coding: utf-8 -*-
 import os
+import re
+import sys
 from setuptools import setup, find_packages
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-name = "zc.recipe.egg"
+def here(name):
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        name)
+
+
+def read(name, mode='rt'):
+    with open(here(name), mode) as fp:
+        return fp.read()
+
+
+def _get_version_str(file_path):
+    version_file = read(file_path)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise ValueError("Unable to find version string.")
+
+
+def find_version(path, pattern='.*\.py$'):
+    regx = re.compile(pattern)
+    for root, dirs, files in os.walk(path):  # 3.5... orz
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            if regx.match(filepath):
+                try:
+                    return _get_version_str(filepath)
+                except ValueError:
+                    pass  # next
+    else:
+        raise ValueError('Version file not found: {}'.format(path))
+
+
+# comat libraries
+version_require = []
+if sys.version_info < (3, 4, 0):
+    version_require.append('enum34')
+
+if sys.version_info < (3, 3, 0):
+    version_require.append('repoze.lru')
+
+
 setup(
-    name = name,
-    version = version,
-    author = "Jim Fulton",
-    author_email = "jim@zope.com",
-    description = "Recipe for installing Python package distributions as eggs",
-    long_description = (
-        read('README.rst')
-        + '\n' +
-        read('CHANGES.rst')
-        + '\n' +
-        'Detailed Documentation\n'
-        '**********************\n'
-        + '\n' +
-        read('src', 'zc', 'recipe', 'egg', 'README.rst')
-        + '\n' +
-        read('src', 'zc', 'recipe', 'egg', 'custom.rst')
-        + '\n' +
-        read('src', 'zc', 'recipe', 'egg', 'api.rst')
-        + '\n' +
-        'Download\n'
-        '*********\n'
-        ),
+    name='sximada.recipe.requirements',
+    version=find_version('src'),
+    description='Python package boilerplate',
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
     keywords = "development build",
     classifiers = [
        'Development Status :: 5 - Production/Stable',
@@ -61,26 +69,60 @@ setup(
        'Programming Language :: Python :: 3.3',
        'Topic :: Software Development :: Build Tools',
        'Topic :: Software Development :: Libraries :: Python Modules',
-       ],
-    url='http://cheeseshop.python.org/pypi/zc.recipe.egg',
-    license = "ZPL 2.1",
-
-    packages = find_packages('src'),
-    package_dir = {'':'src'},
-    namespace_packages = ['zc', 'zc.recipe'],
-    install_requires = [
-        'zc.buildout >=1.2.0',
-        'setuptools'],
-    tests_require = ['zope.testing'],
-    test_suite = name+'.tests.test_suite',
-    entry_points = {'zc.buildout': ['default = %s:Scripts' % name,
-                                    'script = %s:Scripts' % name,
-                                    'scripts = %s:Scripts' % name,
-                                    'eggs = %s:Eggs' % name,
-                                    'custom = %s:Custom' % name,
-                                    'develop = %s:Develop' % name,
-                                    ]
-                    },
-    include_package_data = True,
+    ],
+    author='TakesxiSximada',
+    author_email='sximada@gmail.com',
+    url='https://github.com/TakesxiSximada/sximada.recipe.requirements',
+    packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={'': 'src'},
+    namespace_packages=['sximada'],
+    include_package_data=True,
     zip_safe=False,
+    test_suite='nose.collector',
+    install_requires=[
+        'six',
+        'zope.interface',
+        'lazr.delegates',
+    ] + version_require,
+    tests_require=[
+        'astroid',  # use by pylint
+        'eventlet',  # use by detox
+        'flake8',
+        'vulture',
+        'pylint',
+        'nose',
+        'mypy-lang',
+        'tox',
+        'detox',
+        'selenium',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx',
+        ],
+        'develop': [
+            'wheel',
+        ],
+        'testing': [
+            'astroid',  # use by pylint
+            'eventlet',  # use by detox
+            'flake8',
+            'vulture',
+            'pylint',
+            'nose',
+            'mypy-lang',
+            'tox',
+            'detox',
+            'selenium',
+        ],
+    },
+    dependency_links=[
+        'https://bitbucket.org/hpk42/detox/get/0.9.4.zip#eggs=detox',
+        'https://bitbucket.org/logilab/astroid/get/tip.zip#eggs=astroid',
+        'https://github.com/eventlet/eventlet/archive/master.zip#eggs=eventlet',
+    ],
+    entry_points="""\
+      [console_scripts]
+      sximada-boilerplate = sximada.boilerplate.commands:main
+      """,
     )
