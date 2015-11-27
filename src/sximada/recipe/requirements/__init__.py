1571ca09d2cfb066098d6bb8d6280c0c04f1b29e
TakesxiSximada <takesxi.sximada@gmail.com>
2015-11-27 19:06:23 +0900

    rel import

--- src/sximada/recipe/requirements/__init__.py
+++ src/sximada/recipe/requirements/__init__.py
@@ -1,2 +1,6 @@
-from zc.recipe.egg.egg import Egg, Scripts, Eggs
-from zc.recipe.egg.custom import Custom, Develop
+# -*- coding: utf-8 -*-
+from .recipe.egg.egg import Egg, Scripts, Eggs
+from .recipe.egg.custom import Custom, Develop
+
+
+__version__ = '0.1.dev0'
